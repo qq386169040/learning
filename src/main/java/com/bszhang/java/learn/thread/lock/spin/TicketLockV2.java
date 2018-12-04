d12ab54d0b40d8c15de2afe0f474e7a72c200295
bszhang <bszhang@sysnew.com>
2018-12-04 08:06:21 +0800

    TicketLock V1/2

--- src/main/java/com/bszhang/java/learn/thread/lock/spin/TicketLockV2.java
+++ src/main/java/com/bszhang/java/learn/thread/lock/spin/TicketLockV2.java
@@ -2,27 +2,30 @@ package com.bszhang.java.learn.thread.lock.spin;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class TicketLock {
+public class TicketLockV2 {
     // 1
     private AtomicInteger serviceNum = new AtomicInteger();
     // 0
     private AtomicInteger ticketNum = new AtomicInteger();
+    private ThreadLocal<Integer> ticketNumberHolder = new ThreadLocal<Integer>();
 
-    public int lock() {
+    public void lock() {
         // 1
         // 2
         int currentTicketNum = ticketNum.incrementAndGet();
+        // 1
+        // 2
+        ticketNumberHolder.set(currentTicketNum);
         // 1 != 1
         // 2 != 1
-        // 2 != 2
         while (currentTicketNum != serviceNum.get()) {
             // Do nothing
         }
-        return currentTicketNum;
     }
 
-    public void unlock(int ticketNum) {
+    public void unlock() {
         // 2
-        serviceNum.compareAndSet(ticketNum, ticketNum + 1);
+        Integer currentTickNum = ticketNumberHolder.get();
+        serviceNum.compareAndSet(currentTickNum, currentTickNum + 1);
     }
 }

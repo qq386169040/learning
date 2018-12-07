8ed57d52fc27f5bb74c6cf28d985f91f9adee41e
bszhang <bszhang@sysnew.com>
2018-12-07 09:01:57 +0800

    parseArgs

--- src/main/java/com/bszhang/java/learn/clean/code/Args.java
+++ src/main/java/com/bszhang/java/learn/clean/code/Args.java
@@ -1,22 +1,51 @@
 package com.bszhang.java.learn.clean.code;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Args {
     private String schema;
     private String[] args;
 
     private Map<Character, Boolean> booleanMap = new HashMap<Character, Boolean>();
+    private Set<Character> flagSet = new HashSet<Character>();
 
-    public Args(String schema, String args[]) {
+    public Args(String schema, String[] args) {
         this.schema = schema;
         this.args = args;
         parse();
     }
 
     private void parse() {
+        initFlagSet();
+        parseArgs();
+    }
 
+    private void parseArgs() {
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+            if (!arg.startsWith("-")) {
+                throw new RuntimeException("格式错误，每个参数需要以-<flag>开始");
+            }
+
+            arg = arg.substring(1);
+            for (int j = 0; j < arg.length(); j++) {
+                booleanMap.put(arg.charAt(j), true);
+            }
+
+            if ((i < args.length -1) && !args[i + 1].startsWith("-")) {
+                i++;
+            }
+        }
+    }
+
+    private void initFlagSet() {
+        String[] flagArray = schema.split(",");
+        for (String flag : flagArray) {
+            flagSet.add(flag.charAt(0));
+        }
     }
 
     public boolean getBoolean(char arg) {
@@ -31,6 +60,6 @@ public class Args {
 
         assert (arg.getBoolean('l'));
         assert (arg.getBoolean('d'));
-        assert (arg.getBoolean('u') == false);
+        assert (!arg.getBoolean('u'));
     }
 }
